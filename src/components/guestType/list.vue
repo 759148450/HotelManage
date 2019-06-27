83749c725a5d8e99733bd0faf6ccf66832245442
zyp <759148450@qq.com>
2019-06-27 16:05:11 +0800

    客房类型，修改删除按钮

--- src/components/guestType/list.vue
+++ src/components/guestType/list.vue
@@ -49,8 +49,10 @@
 
         <el-table-column label="操作">
         <template slot-scope="scope">
-            <el-button @click="edit(scope.row)" type="text" size="small">修改</el-button>
-            <el-button type="text" size="small" @click="del(scope.row)">{{deltext(scope.row.active)}}</el-button>
+            <el-button @click="edit(scope.row)" type="primary" icon="el-icon-edit" circle></el-button>
+            <!--<el-button @click="edit(scope.row)" type="text" size="small" icon="el-icon-edit" circle>修改</el-button>-->
+          <el-button @click="del(scope.row)" type="danger" icon="el-icon-delete" circle></el-button>
+            <!--<el-button type="text" size="small" @click="del(scope.row)">{{deltext(scope.row.active)}}</el-button>-->
         </template>
         </el-table-column>
     </el-table>
